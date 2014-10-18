3ec8fd8e5f4f9a7e9592e7e1b9c9cf4cf6390a2e
guilherme <gobbi9@live.com>
2014-10-17 23:44:03 -0300

    alterações na estrutura do bd

--- arquivos/VoceAlugaEstrutura.sql
+++ arquivos/VoceAlugaEstrutura.sql
@@ -7,12 +7,10 @@ CREATE TABLE carro (
  necessita_de_conserto boolean,
  quilometragem DOUBLE PRECISION,
  ultima_revisao DATE,
- manutencao_no_dia boolean,
  filial text,
  ano VARCHAR(4),
  id_locacao BIGINT UNSIGNED,
  id_reserva BIGINT UNSIGNED,
- id_cliente BIGINT UNSIGNED,
  id_manutencao BIGINT UNSIGNED
 );
 
@@ -26,7 +24,6 @@ CREATE TABLE cliente (
  lista_negra boolean,
  id_locacao BIGINT UNSIGNED,
  id_reserva BIGINT UNSIGNED,
- id_carro BIGINT UNSIGNED,
  id_motorista BIGINT UNSIGNED
 );
 
@@ -77,7 +74,8 @@ CREATE TABLE pagamento (
  tarifa_de_atraso DOUBLE PRECISION,
  forma_de_pagamento text,
  data DATE,
- id_locacao BIGINT UNSIGNED
+ id_locacao BIGINT UNSIGNED,
+ id_reserva BIGINT UNSIGNED
 );
 
 ALTER TABLE pagamento ADD CONSTRAINT PK_pagamento PRIMARY KEY (id_pagamento);
@@ -86,34 +84,23 @@ ALTER TABLE pagamento ADD CONSTRAINT PK_pagamento PRIMARY KEY (id_pagamento);
 CREATE TABLE reserva (
  id_reserva SERIAL,
  data_fim DATE,
- dataInicio DATE,
+ data_inicio DATE,
  id_locacao BIGINT UNSIGNED,
  id_cliente BIGINT UNSIGNED,
- id_carro BIGINT UNSIGNED
+ id_carro BIGINT UNSIGNED,
+ id_pagamento BIGINT UNSIGNED
 );
 
 ALTER TABLE reserva ADD CONSTRAINT PK_reserva PRIMARY KEY (id_reserva);
 
 
-CREATE TABLE pagamento_tem_reserva (
- id_pagamento_tem_reserva SERIAL,
- id_pagamento BIGINT UNSIGNED,
- id_reserva BIGINT UNSIGNED
-);
-
-ALTER TABLE pagamento_tem_reserva ADD CONSTRAINT PK_pagamento_tem_reserva PRIMARY KEY (id_pagamento_tem_reserva);
-
-
-
 ALTER TABLE carro ADD CONSTRAINT FK_carro_0 FOREIGN KEY (id_locacao) REFERENCES locacao (id_locacao);
 ALTER TABLE carro ADD CONSTRAINT FK_carro_1 FOREIGN KEY (id_reserva) REFERENCES reserva (id_reserva);
-ALTER TABLE carro ADD CONSTRAINT FK_carro_2 FOREIGN KEY (id_cliente) REFERENCES cliente (id_cliente);
 ALTER TABLE carro ADD CONSTRAINT FK_carro_3 FOREIGN KEY (id_manutencao) REFERENCES manutencao (id_manutencao);
 
 
 ALTER TABLE cliente ADD CONSTRAINT FK_cliente_0 FOREIGN KEY (id_locacao) REFERENCES locacao (id_locacao);
 ALTER TABLE cliente ADD CONSTRAINT FK_cliente_1 FOREIGN KEY (id_reserva) REFERENCES reserva (id_reserva);
-ALTER TABLE cliente ADD CONSTRAINT FK_cliente_2 FOREIGN KEY (id_carro) REFERENCES carro (id_carro);
 ALTER TABLE cliente ADD CONSTRAINT FK_cliente_3 FOREIGN KEY (id_motorista) REFERENCES motorista (id_motorista);
 
 
@@ -130,14 +117,12 @@ ALTER TABLE motorista ADD CONSTRAINT FK_motorista_0 FOREIGN KEY (id_cliente) REF
 
 
 ALTER TABLE pagamento ADD CONSTRAINT FK_pagamento_0 FOREIGN KEY (id_locacao) REFERENCES locacao (id_locacao);
+ALTER TABLE pagamento ADD CONSTRAINT FK_pagamento_1 FOREIGN KEY (id_reserva) REFERENCES reserva (id_reserva);
 
 
 ALTER TABLE reserva ADD CONSTRAINT FK_reserva_0 FOREIGN KEY (id_locacao) REFERENCES locacao (id_locacao);
 ALTER TABLE reserva ADD CONSTRAINT FK_reserva_1 FOREIGN KEY (id_cliente) REFERENCES cliente (id_cliente);
 ALTER TABLE reserva ADD CONSTRAINT FK_reserva_2 FOREIGN KEY (id_carro) REFERENCES carro (id_carro);
-
-
-ALTER TABLE pagamento_tem_reserva ADD CONSTRAINT FK_pagamento_tem_reserva_0 FOREIGN KEY (id_pagamento) REFERENCES pagamento (id_pagamento);
-ALTER TABLE pagamento_tem_reserva ADD CONSTRAINT FK_pagamento_tem_reserva_1 FOREIGN KEY (id_reserva) REFERENCES reserva (id_reserva);
+ALTER TABLE reserva ADD CONSTRAINT FK_reserva_3 FOREIGN KEY (id_pagamento) REFERENCES pagamento (id_pagamento);
 
 
